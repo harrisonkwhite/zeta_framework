c47ec6b9d9474a9df724184ed36754265a80722c
Harrison White <harrisonkwhite@gmail.com>
2025-11-07 21:46:17 +1000

    Reimplement foundation of GFX resource arena and texture setup (but with OpenGL).

--- zf/include/zf/rendering.h
+++ zf/include/zf/rendering.h
@@ -1,4 +1,53 @@
 #pragma once
 
+#include <glad/glad.h>
+#include <zc/mem/arrays.h>
+#include <zc/math.h>
+#include <zc/gfx.h>
+
 namespace zf {
+    using t_gl_id = GLuint;
+
+    enum class ec_gfx_resource_type {
+        invalid,
+        texture
+    };
+
+    struct s_gfx_resource_handle {
+        ec_gfx_resource_type type = ec_gfx_resource_type::invalid;
+        t_gl_id gl_id = 0;
+    };
+
+    class c_gfx_resource_arena {
+    public:
+        bool Init(c_mem_arena& mem_arena, const int cap);
+        void Release();
+
+        s_gfx_resource_handle AddTexture(const s_rgba_texture& rgba_tex);
+
+    private:
+        c_array<s_gfx_resource_handle> m_hdls;
+        int m_hdls_taken = 0;
+    };
+
+#if 0
+    class c_texture_group {
+    public:
+        bool Load(c_mem_arena& mem_arena, c_gfx_resource_arena& lifetime, const int cnt) {
+            if (!m_hdls.Init(mem_arena, cnt)) {
+                return false;
+            }
+
+            if (!m_sizes.Init(mem_arena, cnt)) {
+                return false;
+            }
+
+            return true;
+        }
+
+    private:
+        c_array<s_gfx_resource_handle> m_hdls;
+        c_array<s_v2<int>> m_sizes;
+    };
+#endif
 }
--- zf/src/rendering.cpp
+++ zf/src/rendering.cpp
@@ -1,4 +1,72 @@
 #include <zf/rendering.h>
 
 namespace zf {
+    static inline s_v2<int> GLTextureSizeLimit() {
+        int size;
+        glGetIntegerv(GL_MAX_TEXTURE_SIZE, &size);
+        return {size, size};
+    }
+
+    static t_gl_id GenGLTextureFromRGBA(const s_rgba_texture& rgba_tex) {
+        const s_v2<int> tex_size_limit = GLTextureSizeLimit();
+
+        if (rgba_tex.size_in_pxs.x > tex_size_limit.x || rgba_tex.size_in_pxs.y > tex_size_limit.y) {
+            ZF_LOG_ERROR("Texture size (%d, %d) exceeds OpenGL limits (%d, %d)!", rgba_tex.size_in_pxs.x, rgba_tex.size_in_pxs.y, tex_size_limit.x, tex_size_limit.y);
+            return 0;
+        }
+
+        t_gl_id tex_gl_id;
+        glGenTextures(1, &tex_gl_id);
+
+        glBindTexture(GL_TEXTURE_2D, tex_gl_id);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgba_tex.size_in_pxs.x, rgba_tex.size_in_pxs.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba_tex.px_data.Raw());
+
+        return tex_gl_id;
+    }
+
+    bool c_gfx_resource_arena::Init(c_mem_arena& mem_arena, const int cap) {
+        ZF_ASSERT(cap > 0);
+
+        m_hdls_taken = 0;
+        return m_hdls.Init(mem_arena, cap);
+    }
+
+    void c_gfx_resource_arena::Release() {
+        for (int i = 0; i < m_hdls_taken; i++) {
+            const auto hdl = m_hdls[i];
+
+            switch (hdl.type) {
+            case ec_gfx_resource_type::texture:
+                glDeleteTextures(1, &hdl.gl_id);
+                break;
+            }
+        }
+    }
+
+    s_gfx_resource_handle c_gfx_resource_arena::AddTexture(const s_rgba_texture& rgba_tex) {
+        if (m_hdls_taken == m_hdls.Len()) {
+            return {};
+        }
+
+        const t_gl_id tex_gl_id = GenGLTextureFromRGBA(rgba_tex);
+
+        if (!tex_gl_id) {
+            return {};
+        }
+
+        auto& hdl = m_hdls[m_hdls_taken];
+
+        hdl = {
+            .type = ec_gfx_resource_type::texture,
+            .gl_id = tex_gl_id
+        };
+
+        m_hdls_taken++;
+
+        return hdl;
+    }
 }
