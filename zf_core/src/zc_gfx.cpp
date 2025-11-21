2c61e48c2fce68c42bd3030d0e323e733ac58dbe
Harrison White <harrisonkwhite@gmail.com>
2025-11-21 12:39:36 +1000

    Make start on Unicode character support with rework of old font arrangement loading code.

--- zf_core/src/zc_gfx.cpp
+++ zf_core/src/zc_gfx.cpp
@@ -86,4 +86,111 @@ namespace zf {
 
         return success;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    struct s_font_arrangement {
+        t_s32 line_height;
+
+        s_array<s_v2<t_s32>> chr_offsets;
+        s_array<s_v2<t_s32>> chr_sizes;
+        s_array<t_s32> chr_advances;
+
+        // @todo: Also need kerning pairs, only keep the non-0 ones and index into it with a hash map.
+    };
+
+    [[nodiscard]] static t_b8 LoadFontArrangement(s_mem_arena& mem_arena, const stbtt_fontinfo& stb_font_info, const t_s32 height, const s_array_rdonly<t_s32> utf_codepoints, s_font_arrangement& o_arrangement) {
+        ZF_ASSERT(!IsArrayEmpty(utf_codepoints));
+
+        if (!MakeArray(mem_arena, utf_codepoints.len, o_arrangement.chr_offsets)) {
+            return false;
+        }
+
+        if (!MakeArray(mem_arena, utf_codepoints.len, o_arrangement.chr_sizes)) {
+            return false;
+        }
+
+        if (!MakeArray(mem_arena, utf_codepoints.len, o_arrangement.chr_advances)) {
+            return false;
+        }
+
+        const t_f32 scale = stbtt_ScaleForPixelHeight(&stb_font_info, static_cast<t_f32>(height)); // @todo: Why the hell is the height a float?
+
+        t_s32 vm_ascent, vm_descent, vm_line_gap;
+        stbtt_GetFontVMetrics(&stb_font_info, &vm_ascent, &vm_descent, &vm_line_gap);
+
+        o_arrangement.line_height = static_cast<t_s32>(static_cast<t_f32>(vm_ascent - vm_descent + vm_line_gap) * scale);
+
+        for (t_size i = 0; i < utf_codepoints.len; i++) {
+            // @todo: Handle case where codepoint is unsupported.
+            // @todo: We need kerning!
+
+            // @todo: Apparently this doesn't always give a tight box? Test with CJK glyphs.
+            t_s32 bm_box_left, bm_box_top, bm_box_right, bm_box_bottom;
+            stbtt_GetCodepointBitmapBox(&stb_font_info, utf_codepoints[i], scale, scale, &bm_box_left, &bm_box_top, &bm_box_right, &bm_box_bottom);
+
+            o_arrangement.chr_offsets[i] = {
+                bm_box_left,
+                bm_box_top + static_cast<t_s32>(static_cast<t_f32>(vm_ascent) * scale)
+            };
+
+            o_arrangement.chr_sizes[i] = {
+                bm_box_right - bm_box_left, bm_box_bottom - bm_box_top
+            };
+
+            t_s32 hm_advance;
+            stbtt_GetCodepointHMetrics(&stb_font_info, utf_codepoints[i], &hm_advance, nullptr);
+
+            o_arrangement.chr_advances[i] = static_cast<t_s32>(static_cast<t_f32>(hm_advance) * scale);
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
