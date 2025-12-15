c8db4b3be2f85b7a470aba648dd6d0bfaf130403
Harrison White <harrisonkwhite@gmail.com>
2025-12-16 08:44:25 +1000

    Drop texture code in preparation for BGFX substitution.

--- zf_game_lib/src/zgl_rendering.cpp
+++ zf_game_lib/src/zgl_rendering.cpp
@@ -8,13 +8,12 @@ namespace zf {
     struct s_batch_vert {
         s_v2 pos;
         s_color_rgba32f blend;
-        s_v2 uv;
 
         s_batch_vert() = default;
-        s_batch_vert(const s_v2 pos, const s_color_rgba32f blend, const s_v2 uv) : pos(pos), blend(blend), uv(uv) {}
+        s_batch_vert(const s_v2 pos, const s_color_rgba32f blend) : pos(pos), blend(blend) {}
     };
 
-    constexpr s_static_array<t_i32, 3> g_batch_vert_attr_component_cnts = {2, 4, 2}; // This has to match the number of components per attribute above.
+    constexpr s_static_array<t_i32, 2> g_batch_vert_attr_component_cnts = {2, 4}; // This has to match the number of components per attribute above.
 
     constexpr t_len g_batch_vert_component_cnt = ZF_SIZE_OF(s_batch_vert) / ZF_SIZE_OF(t_f32);
 
@@ -71,12 +70,6 @@ void main() {
             ZF_FATAL();
         }
 
-        constexpr s_static_array<t_u8, 4> white_px_tex_rgba = {255, 255, 255, 255};
-
-        if (!CreateTexture({{1, 1}, white_px_tex_rgba}, basis.gfx_res_arena, basis.white_px_texture)) {
-            ZF_FATAL();
-        }
-
         return basis;
     }
 
@@ -88,7 +81,6 @@ void main() {
         s_render_instr_seq instr_seq;
 
         s_static_list<s_batch_vert, g_batch_vert_limit> batch_verts;
-        s_ptr<const s_gfx_resource> batch_texture;
     };
 
     s_rendering_state &internal::BeginRendering(const s_rendering_basis &basis, s_mem_arena &mem_arena) {
@@ -120,9 +112,7 @@ void main() {
 
         rs.instr_seq.SubmitShaderProgUniformSet(s_cstr_literal("u_proj"), proj_mat);
 
-        rs.instr_seq.SubmitMeshDraw(*rs.basis->batch_mesh, rs.batch_texture ? rs.batch_texture : rs.basis->white_px_texture);
-
-        rs.batch_texture = nullptr;
+        rs.instr_seq.SubmitMeshDraw(*rs.basis->batch_mesh);
     }
 
     void internal::EndRendering(s_rendering_state &rs, s_mem_arena &temp_mem_arena) {
@@ -140,39 +130,38 @@ void main() {
             Flush(rs);
         }
 
-        rs.batch_verts.Append({pts[0], pt_colors[0], {}});
-        rs.batch_verts.Append({pts[1], pt_colors[1], {}});
-        rs.batch_verts.Append({pts[2], pt_colors[2], {}});
+        rs.batch_verts.Append({pts[0], pt_colors[0]});
+        rs.batch_verts.Append({pts[1], pt_colors[1]});
+        rs.batch_verts.Append({pts[2], pt_colors[2]});
     }
 
     void DrawRect(s_rendering_state &rs, const s_rect_f rect, const s_color_rgba32f color_topleft, const s_color_rgba32f color_topright, const s_color_rgba32f color_bottomright, const s_color_rgba32f color_bottomleft) {
-        if (rs.batch_verts.Len() + 6 > rs.batch_verts.Cap() || rs.batch_texture) {
+        if (rs.batch_verts.Len() + 6 > rs.batch_verts.Cap()) {
             Flush(rs);
         }
 
-        rs.batch_verts.Append({rect.TopLeft(), color_topleft, {}});
-        rs.batch_verts.Append({rect.TopRight(), color_topright, {}});
-        rs.batch_verts.Append({rect.BottomRight(), color_bottomright, {}});
+        rs.batch_verts.Append({rect.TopLeft(), color_topleft});
+        rs.batch_verts.Append({rect.TopRight(), color_topright});
+        rs.batch_verts.Append({rect.BottomRight(), color_bottomright});
 
-        rs.batch_verts.Append({rect.BottomRight(), color_bottomright, {}});
-        rs.batch_verts.Append({rect.BottomLeft(), color_bottomleft, {}});
-        rs.batch_verts.Append({rect.TopLeft(), color_topleft, {}});
+        rs.batch_verts.Append({rect.BottomRight(), color_bottomright});
+        rs.batch_verts.Append({rect.BottomLeft(), color_bottomleft});
+        rs.batch_verts.Append({rect.TopLeft(), color_topleft});
     }
 
     void DrawTexture(s_rendering_state &rs, const s_gfx_resource &tex, const s_v2 pos, const s_color_rgba32f blend) {
-        if (rs.batch_verts.Len() + 6 > rs.batch_verts.Cap()
-            || (!rs.batch_verts.IsEmpty() && rs.batch_texture != s_ptr<const s_gfx_resource>(&tex))) {
+        if (rs.batch_verts.Len() + 6 > rs.batch_verts.Cap()) {
             Flush(rs);
         }
 
         const s_rect_f rect = {pos, TextureSize(tex).ToV2()};
 
-        rs.batch_verts.Append({rect.TopLeft(), blend, {0.0f, 0.0f}});
-        rs.batch_verts.Append({rect.TopRight(), blend, {1.0f, 0.0f}});
-        rs.batch_verts.Append({rect.BottomRight(), blend, {1.0f, 1.0f}});
+        rs.batch_verts.Append({rect.TopLeft(), blend});
+        rs.batch_verts.Append({rect.TopRight(), blend});
+        rs.batch_verts.Append({rect.BottomRight(), blend});
 
-        rs.batch_verts.Append({rect.BottomRight(), blend, {1.0f, 1.0f}});
-        rs.batch_verts.Append({rect.BottomLeft(), blend, {0.0f, 1.0f}});
-        rs.batch_verts.Append({rect.TopLeft(), blend, {0.0f, 0.0f}});
+        rs.batch_verts.Append({rect.BottomRight(), blend});
+        rs.batch_verts.Append({rect.BottomLeft(), blend});
+        rs.batch_verts.Append({rect.TopLeft(), blend});
     }
 }
