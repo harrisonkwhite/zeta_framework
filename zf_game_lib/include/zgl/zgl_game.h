bbe634dbc5a8be2bd5a49f01c4bf1f7de4dc5386
Harrison White <harrisonkwhite@gmail.com>
2026-01-12 12:42:19 +1000

    In ZGL make sure types from other headers are only forward-declared.

--- zf_game_lib/include/zgl/zgl_game.h
+++ zf_game_lib/include/zgl/zgl_game.h
@@ -1,85 +1,94 @@
 #pragma once
 
 #include <zcl.h>
-#include <zgl/zgl_input.h>
-#include <zgl/zgl_gfx.h>
 
 namespace zgl::game {
-    // ============================================================
-    // @section: Types and Constants
-
-    struct t_init_func_context {
-        zcl::mem::t_arena *perm_arena;
-        zcl::mem::t_arena *temp_arena;
-
-        gfx::t_resource_group *perm_gfx_resource_group;
-
-        zcl::rand::t_rng *rng;
-
-        void *user_mem;
-    };
-
-    struct t_tick_func_context {
-        zcl::mem::t_arena *perm_arena;
-        zcl::mem::t_arena *temp_arena;
-
-        const input::t_state *input_state;
-
-        gfx::t_resource_group *perm_gfx_resource_group;
-
-        zcl::rand::t_rng *rng;
-
-        zcl::t_f64 fps;
-
-        void *user_mem;
-    };
-
-    struct t_render_func_context {
-        zcl::mem::t_arena *perm_arena;
-        zcl::mem::t_arena *temp_arena;
-
-        gfx::t_frame_context *frame_context;
-
-        zcl::rand::t_rng *rng;
-
-        zcl::t_f64 fps;
-
-        void *user_mem;
-    };
-
-    using t_init_func = void (*)(const t_init_func_context &context);
-    using t_deinit_func = void (*)(void *const user_mem);
-    using t_tick_func = void (*)(const t_tick_func_context &context);
-    using t_render_func = void (*)(const t_render_func_context &context);
-
-    struct t_config {
-        t_init_func init_func;
-        t_deinit_func deinit_func;
-        t_tick_func tick_func;
-        t_render_func render_func;
-
-        zcl::t_i32 user_mem_size;
-        zcl::t_i32 user_mem_alignment;
-    };
-
-    // ============================================================
-
-
-    // ============================================================
-    // @section: Functions
-
-    void run(const t_config &config);
-
-    inline void config_assert_valid(const t_config &config) {
-        ZF_ASSERT(config.init_func);
-        ZF_ASSERT(config.deinit_func);
-        ZF_ASSERT(config.tick_func);
-        ZF_ASSERT(config.render_func);
-
-        ZF_ASSERT((config.user_mem_size == 0 && config.user_mem_alignment == 0) || (config.user_mem_size > 0 && zcl::mem::alignment_check_valid(config.user_mem_alignment)));
+    namespace input {
+        struct t_state;
     }
 
-    void tps_set_target(const zcl::t_f64 tps);
+    namespace gfx {
+        struct t_resource_group;
+        struct t_frame_context;
+    }
 
-    // ============================================================
+    namespace game {
+        // ============================================================
+        // @section: Types and Constants
+
+        struct t_init_func_context {
+            zcl::mem::t_arena *perm_arena;
+            zcl::mem::t_arena *temp_arena;
+
+            gfx::t_resource_group *perm_gfx_resource_group;
+
+            zcl::rand::t_rng *rng;
+
+            void *user_mem;
+        };
+
+        struct t_tick_func_context {
+            zcl::mem::t_arena *perm_arena;
+            zcl::mem::t_arena *temp_arena;
+
+            const input::t_state *input_state;
+
+            gfx::t_resource_group *perm_gfx_resource_group;
+
+            zcl::rand::t_rng *rng;
+
+            zcl::t_f64 fps;
+
+            void *user_mem;
+        };
+
+        struct t_render_func_context {
+            zcl::mem::t_arena *perm_arena;
+            zcl::mem::t_arena *temp_arena;
+
+            gfx::t_frame_context *frame_context;
+
+            zcl::rand::t_rng *rng;
+
+            zcl::t_f64 fps;
+
+            void *user_mem;
+        };
+
+        using t_init_func = void (*)(const t_init_func_context &context);
+        using t_deinit_func = void (*)(void *const user_mem);
+        using t_tick_func = void (*)(const t_tick_func_context &context);
+        using t_render_func = void (*)(const t_render_func_context &context);
+
+        struct t_config {
+            t_init_func init_func;
+            t_deinit_func deinit_func;
+            t_tick_func tick_func;
+            t_render_func render_func;
+
+            zcl::t_i32 user_mem_size;
+            zcl::t_i32 user_mem_alignment;
+        };
+
+        // ============================================================
+
+
+        // ============================================================
+        // @section: Functions
+
+        void run(const t_config &config);
+
+        inline void config_assert_valid(const t_config &config) {
+            ZF_ASSERT(config.init_func);
+            ZF_ASSERT(config.deinit_func);
+            ZF_ASSERT(config.tick_func);
+            ZF_ASSERT(config.render_func);
+
+            ZF_ASSERT((config.user_mem_size == 0 && config.user_mem_alignment == 0) || (config.user_mem_size > 0 && zcl::mem::alignment_check_valid(config.user_mem_alignment)));
+        }
+
+        void tps_set_target(const zcl::t_f64 tps);
+
+        // ============================================================
+    }
 }
--- zf_game_lib/include/zgl/zgl_platform.h
+++ zf_game_lib/include/zgl/zgl_platform.h
@@ -1,54 +1,59 @@
 #pragma once
 
 #include <zcl.h>
-#include <zgl/zgl_input.h>
 
-namespace zgl::platform {
-    // Note that the window is not shown by default, you have to manually do this.
-    void module_startup(const zcl::math::t_v2_i init_window_size);
-
-    void module_shutdown();
-
-    // Gives the time in seconds since the platform module was started.
-    zcl::t_f64 get_time();
-
-    // Also updates the given input state based on events.
-    void poll_events(input::t_state *const input_state);
-
-    void *display_get_native_handle();
-
-    void *window_get_native_handle();
-
-    void window_show();
-
-    void window_request_close();
-    zcl::t_b8 window_check_close_requested();
-
-    void window_set_title(const zcl::strs::t_str_rdonly title, zcl::mem::t_arena *const temp_arena);
-
-    // Sets the LOGICAL window size. The actual new framebuffer size MIGHT be larger if there is DPI scaling.
-    void window_set_size(const zcl::math::t_v2_i size);
-
-    // Set the LOGICAL window size limits. If you don't want to limit a particular dimension, leave it as -1.
-    void window_set_size_limits(const zcl::t_i32 min_width, const zcl::t_i32 min_height, const zcl::t_i32 max_width, const zcl::t_i32 max_height);
-
-    void window_set_resizable(const zcl::t_b8 resizable);
-
-    zcl::math::t_v2_i window_get_framebuffer_size_cache();
-
-    zcl::t_b8 window_check_fullscreen();
-
-    void window_set_fullscreen(const zcl::t_b8 active);
-
-    inline void window_toggle_fullscreen() {
-        window_set_fullscreen(!window_check_fullscreen());
+namespace zgl {
+    namespace input {
+        struct t_state;
     }
 
-    // Calculates the size in pixels of whichever monitor the window most resides in.
-    zcl::math::t_v2_i monitor_calc_size_pixels();
+    namespace platform {
+        // Note that the window is not shown by default, you have to manually do this.
+        void module_startup(const zcl::math::t_v2_i init_window_size);
 
-    // Calculates the size (accounting for DPI scaling) of whichever monitor the window most resides in.
-    zcl::math::t_v2_i monitor_calc_size_logical();
+        void module_shutdown();
 
-    void cursor_set_visible(const zcl::t_b8 visible);
+        // Gives the time in seconds since the platform module was started.
+        zcl::t_f64 get_time();
+
+        // Also updates the given input state based on events.
+        void poll_events(input::t_state *const input_state);
+
+        void *display_get_native_handle();
+
+        void *window_get_native_handle();
+
+        void window_show();
+
+        void window_request_close();
+        zcl::t_b8 window_check_close_requested();
+
+        void window_set_title(const zcl::strs::t_str_rdonly title, zcl::mem::t_arena *const temp_arena);
+
+        // Sets the LOGICAL window size. The actual new framebuffer size MIGHT be larger if there is DPI scaling.
+        void window_set_size(const zcl::math::t_v2_i size);
+
+        // Set the LOGICAL window size limits. If you don't want to limit a particular dimension, leave it as -1.
+        void window_set_size_limits(const zcl::t_i32 min_width, const zcl::t_i32 min_height, const zcl::t_i32 max_width, const zcl::t_i32 max_height);
+
+        void window_set_resizable(const zcl::t_b8 resizable);
+
+        zcl::math::t_v2_i window_get_framebuffer_size_cache();
+
+        zcl::t_b8 window_check_fullscreen();
+
+        void window_set_fullscreen(const zcl::t_b8 active);
+
+        inline void window_toggle_fullscreen() {
+            window_set_fullscreen(!window_check_fullscreen());
+        }
+
+        // Calculates the size in pixels of whichever monitor the window most resides in.
+        zcl::math::t_v2_i monitor_calc_size_pixels();
+
+        // Calculates the size (accounting for DPI scaling) of whichever monitor the window most resides in.
+        zcl::math::t_v2_i monitor_calc_size_logical();
+
+        void cursor_set_visible(const zcl::t_b8 visible);
+    }
 }
