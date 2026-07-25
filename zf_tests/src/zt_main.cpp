96c2249318653941a3eaafe9f0962f4382340a49
Harrison White <harrisonkwhite@gmail.com>
2026-07-25 23:01:09 +1000

    Add tests for all sort helper functions.

--- zf_tests/src/zt_main.cpp
+++ zf_tests/src/zt_main.cpp
@@ -1,15 +1,72 @@
 #include <zcl.h>
 
 static void TestSorting(zcl::t_rng *const rng, zcl::t_arena *const temp_arena) {
-    zcl::t_static_array<zcl::t_i32, 32> nums = {};
+    enum t_sort_type {
+        ek_sort_type_bubble,
+        ek_sort_type_insertion,
+        ek_sort_type_selection,
+        ek_sort_type_merge,
+        ek_sort_type_quick,
 
-    for (zcl::t_i32 i = 0; i < nums.k_len; i++) {
-        nums[i] = zcl::RandGenI32(rng);
+        ekm_sort_type_cnt,
+    };
+
+    const auto nums_generator = [rng, temp_arena](const zcl::t_i32 len) -> zcl::t_array_mut<zcl::t_i32> {
+        ZCL_ASSERT(len >= 0);
+
+        if (len == 0) {
+            return {};
+        }
+
+        const auto result = zcl::ArenaPushArray<zcl::t_i32>(temp_arena, len);
+
+        for (zcl::t_i32 i = 0; i < result.len; i++) {
+            result[i] = zcl::RandGenI32(rng);
+        }
+
+        return result;
+    };
+
+    for (zcl::t_i32 i = 0; i < ekm_sort_type_cnt; i++) {
+        for (zcl::t_i32 len = 0; len < 128; len++) {
+            const auto nums = nums_generator(len);
+
+            switch (static_cast<t_sort_type>(i)) {
+                case ek_sort_type_bubble: {
+                    zcl::RunBubbleSort(nums);
+                    break;
+                }
+
+                case ek_sort_type_insertion: {
+                    zcl::RunInsertionSort(nums);
+                    break;
+                }
+
+                case ek_sort_type_selection: {
+                    zcl::RunSelectionSort(nums);
+                    break;
+                }
+
+                case ek_sort_type_merge: {
+                    zcl::RunMergeSort(nums, temp_arena);
+                    break;
+                }
+
+                case ek_sort_type_quick: {
+                    zcl::RunQuickSort(nums);
+                    break;
+                }
+
+                case ekm_sort_type_cnt: {
+                    ZCL_UNREACHABLE();
+                }
+            }
+
+            ZCL_REQUIRE(zcl::CheckSorted(nums));
+
+            zcl::ArenaRewind(temp_arena);
+        }
     }
-
-    zcl::RunBubbleSort(zcl::ArrayToNonstatic(&nums));
-
-    ZCL_REQUIRE(zcl::CheckSorted(zcl::ArrayToNonstatic(&nums)));
 }
 
 static void TestList(zcl::t_rng *const rng, zcl::t_arena *const temp_arena) {
