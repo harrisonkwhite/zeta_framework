f9b4688d70a4d2cb9c5ce74c4a080ae439817ef0
Harrison White <harrisonkwhite@gmail.com>
2026-07-25 17:53:54 +1000

    Switch to ZF strings for the test data structs.

--- zf_tests/src/zt_main.cpp
+++ zf_tests/src/zt_main.cpp
@@ -1,44 +1,44 @@
 #include <zcl.h>
 
-static zcl::t_b8 TestBits(zcl::t_arena *const arena) {
+static zcl::t_b8 TestBits(zcl::t_arena *const temp_arena) {
     // @todo
     return true;
 }
 
-static zcl::t_b8 TestSorting(zcl::t_arena *const arena) {
+static zcl::t_b8 TestSorting(zcl::t_arena *const temp_arena) {
     // @todo
     return true;
 }
 
-static zcl::t_b8 TestList(zcl::t_arena *const arena) {
+static zcl::t_b8 TestList(zcl::t_arena *const temp_arena) {
     // @todo
     return true;
 }
 
-static zcl::t_b8 TestHashMap(zcl::t_arena *const arena) {
+static zcl::t_b8 TestHashMap(zcl::t_arena *const temp_arena) {
     // @todo
     return true;
 }
 
 struct t_test {
-    const char *title_c_str;
-    zcl::t_b8 (*func)(zcl::t_arena *const arena);
+    zcl::t_str_rdonly title;
+    zcl::t_b8 (*func)(zcl::t_arena *const temp_arena);
 };
 
-constexpr zcl::t_static_array<t_test, 4> k_tests = {{
-    {.title_c_str = "Bits", .func = TestBits},
-    {.title_c_str = "Sorting", .func = TestSorting},
-    {.title_c_str = "List", .func = TestList},
-    {.title_c_str = "Hash Map", .func = TestHashMap},
+static const zcl::t_static_array<t_test, 4> g_tests = {{
+    {.title = ZCL_STR_LITERAL("Bits"), .func = TestBits},
+    {.title = ZCL_STR_LITERAL("Sorting"), .func = TestSorting},
+    {.title = ZCL_STR_LITERAL("List"), .func = TestList},
+    {.title = ZCL_STR_LITERAL("Hash Map"), .func = TestHashMap},
 }};
 
 static void RunTests() {
     zcl::t_arena *const arena = zcl::ArenaCreateBlockBased();
     ZCL_DEFER({ zcl::ArenaDestroy(arena); });
 
-    for (zcl::t_i32 i = 0; i < k_tests.k_len; i++) {
-        zcl::Log(ZCL_STR_LITERAL("Running test \"%\"..."), zcl::CStrToStr(k_tests[i].title_c_str));
-        k_tests[i].func(arena);
+    for (zcl::t_i32 i = 0; i < g_tests.k_len; i++) {
+        zcl::Log(ZCL_STR_LITERAL("Running test \"%\"..."), g_tests[i].title);
+        g_tests[i].func(arena);
     }
 }
 
